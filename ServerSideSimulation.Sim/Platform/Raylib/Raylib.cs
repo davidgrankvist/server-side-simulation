16f098dd8acb24aae1af0796ef41b93db9dd82ea
David Grankvist <grankvist.david@gmail.com>
2024-09-07 23:48:53 +0200

    add raylib bindings and example code

---
+++ ServerSideSimulation.Sim/Platform/Raylib/Raylib.cs
@@ -0,0 +1,112 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ServerSideSimulation.Sim.Platform.Raylib
+{
+	using RenderTexture2D = Raylib.Texture;
+
+    internal static class Raylib
+    {
+		// -------------- TYPES --------------
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Color
+		{
+			public byte r;
+			public byte g;
+			public byte b;
+			public byte a;
+        }
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Texture
+		{
+			public uint id;
+			public int width;
+			public int height;
+			public int mipmaps;
+			public int format;
+		};
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Vector2
+		{
+			public float x;
+			public float y;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Rectangle
+		{
+			public float x;
+			public float y;
+			public float width;
+			public float height;
+		}
+
+		// -------------- FUNCTIONS --------------
+
+		[DllImport("raylib.dll")]
+		public static extern void InitWindow(int width, int height, string title);
+
+		[DllImport("raylib.dll")]
+		public static extern void CloseWindow();
+
+		[DllImport("raylib.dll")]
+		public static extern bool WindowShouldClose();
+
+		[DllImport("raylib.dll")]
+		public static extern void SetTargetFPS(int fps);
+
+		[DllImport("raylib.dll")]
+		public static extern void ClearBackground(Color color);
+
+		[DllImport("raylib.dll")]
+		public static extern void BeginDrawing();
+
+		[DllImport("raylib.dll")]
+		public static extern void EndDrawing();
+
+		[DllImport("raylib.dll")]
+		public static extern void BeginTextureMode(RenderTexture2D target);
+
+		[DllImport("raylib.dll")]
+		public static extern void EndTextureMode();
+
+		[DllImport("raylib.dll")]
+		public static extern void DrawRectangle(int posX, int posY, int width, int height, Color color);
+
+		[DllImport("raylib.dll")]
+		public static extern void DrawRectangleRec(Rectangle rec, Color color);
+
+		[DllImport("raylib.dll")]
+		public static extern void DrawRectanglePro(Rectangle rec, Vector2 origin, float rotation, Color color);
+
+		// -------------- CONSTANTS --------------
+
+		public static class Colors
+		{
+			public static readonly Color White = CreateColor(byte.MaxValue, byte.MaxValue, byte.MaxValue);
+			public static readonly Color Black = CreateColor(0, 0, 0);
+			public static readonly Color Red = CreateColor(byte.MaxValue, 0, 0);
+			public static readonly Color Green = CreateColor(0, byte.MaxValue, 0);
+			public static readonly Color Blue = CreateColor(0, 0, byte.MaxValue);
+
+			public static Color CreateColor(byte r, byte g, byte b, byte a)
+			{
+				return new Color
+				{
+					r = r,
+					g = g,
+					b = b,
+					a = a,
+				};
+			}
+
+			public static Color CreateColor(byte r, byte g, byte b)
+			{
+				return CreateColor(r, g, b, byte.MaxValue);
+			}
+		}
+
+	}
+}
--- ServerSideSimulation.Sim/Program.cs
+++ ServerSideSimulation.Sim/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ServerSideSimulation.Sim
+﻿using ServerSideSimulation.Sim.Platform.Raylib;
+
+namespace ServerSideSimulation.Sim
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Raylib.InitWindow(800, 800, "Hello Raylib");
+			Raylib.SetTargetFPS(60);
+
+			var rect = new Raylib.Rectangle
+			{
+				x = 400,
+				y = 400,
+				width = 120,
+				height = 200
+			};
+			var angle = 0f;
+			var origin = new Raylib.Vector2
+			{
+				x = rect.width / 2,
+				y = rect.height / 2,
+			};
+			var angleDelta = 0.5f;
+
+			while (!Raylib.WindowShouldClose())
+			{
+				Raylib.ClearBackground(Raylib.Colors.White);
+				Raylib.BeginDrawing();
+
+				angle += angleDelta;
+				Raylib.DrawRectanglePro(rect, origin, angle, Raylib.Colors.Blue);
+
+				Raylib.EndDrawing();
+			}
+
+			Raylib.CloseWindow();
 		}
 	}
 }
